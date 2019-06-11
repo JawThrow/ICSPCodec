afcea44f88ad4694db75584f8955c710fe49d7bd
Jaw Park <ijai.whiz@gmail.com>
2019-06-11 23:37:46 +0900

    modify for performance measurement

--- source/encoder/encode.cpp
+++ source/encoder/encode.cpp
@@ -3,30 +3,20 @@
 
 extern char filename[256];
 
-//int main(int argc, char *argv[])
-int main(void)
-{
-
-	//int nframes = atoi(argv[1]);
-	//char *imgfname = argv[2];
-	//int QPDC = atoi(argv[3]);
-	//int QPAC = atoi(argv[4]);
-	//int intraPeriod = atoi(argv[5]);
-
-	int nframes = 90;
-	char *imgfname = "football_cif(352X288)_90f.yuv";
-	int QPDC = 16;
-	int QPAC = 16;
-	//int intraPeriod = ALL_INTRA;
-	int intraPeriod = 10;
-	_splitpath(imgfname, NULL, NULL, filename, NULL);
+// argv: [0]:ICSPCodec.exe [1]sequence_fname [2]nframes [3]QPDC [4]QPAC [5]IntraPeroid [6]AVXFlag
+int main(int argc, char *argv[])
+{	
 	
+	char *imgfname = argv[1]; 
+	int nframes = atoi(argv[2]);	
+	int QPDC = atoi(argv[3]);
+	int QPAC = atoi(argv[4]);
+	int intraPeriod = atoi(argv[5]);
+	int AVXFlag = atoi(argv[6]);
+	#define SIMD AVXFlag;
 	IcspCodec icspCodec;
 	icspCodec.init(nframes, imgfname, 352, 288, QPDC, QPAC);
 	icspCodec.encoding(intraPeriod);
 	
 	return 0;
 }
-//akiyo_cif(352X288)_300f.yuv
-//foreman_cif(352X288)_300f.yuv
-//football_cif(352X288)_90f.yuv
