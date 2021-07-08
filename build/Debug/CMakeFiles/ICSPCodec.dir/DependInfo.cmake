c72973d9811c0df99deac75389b847255891d64b
Imjae Park <ijai.whiz@gmail.com>
2021-07-08 21:25:05 +0900

    add build configuration

---
+++ build/Debug/CMakeFiles/ICSPCodec.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jawthrow/project/ICSPCodec/source/encoder/ICSP_Codec_Encoder_source.cpp" "/home/jawthrow/project/ICSPCodec/build/Debug/CMakeFiles/ICSPCodec.dir/source/encoder/ICSP_Codec_Encoder_source.cpp.o"
+  "/home/jawthrow/project/ICSPCodec/source/encoder/encode.cpp" "/home/jawthrow/project/ICSPCodec/build/Debug/CMakeFiles/ICSPCodec.dir/source/encoder/encode.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
